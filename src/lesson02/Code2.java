f7d48cb9d96d70b23ed4827be17faefb8755ae5b
diel <diel-d@web.de>
2023-11-10 11:09:55 +0100

    Initial commit

---
+++ src/lesson02/Code2.java
@@ -0,0 +1,19 @@
+package lesson02;
+
+public class Code2 {
+    public static void main(String[] args) {
+
+        // число 689
+
+        int x = 12;
+        int y = 4;
+        int z = 10;
+
+        int xResult = x/2;
+        int yResult = y*2;
+        int zResult = z-1;
+
+        System.out.println("Число 689" + "->" + xResult + "," + yResult + "," + zResult);
+
+    }
+}
---
+++ src/lesson02/Main.java
@@ -0,0 +1,18 @@
+package lesson02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte x = 2;
+        short y = 38;
+        int z = 90;
+        long q = 35555;
+        float f = 5.5689f;
+        double d = 8.5896;
+        boolean b = true;
+        char c = 'F';
+        System.out.println(" byte:" + x + " short:" + y + " int:" + z + " long:" + q + " float:" + f + " double:" + d + " boolean:" + b + " char:" + c);
+
+
+    }
+}
