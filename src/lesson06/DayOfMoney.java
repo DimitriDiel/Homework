d6c6eebbee526af3cebb5a9134e08402906130ba
diel <diel-d@web.de>
2023-11-20 11:37:34 +0100

    h6

---
+++ src/lesson06/DayOfMoney.java
@@ -0,0 +1,155 @@
+package lesson06;
+
+public class DayOfMoney {
+
+    public void MoneyFlow(double n) {
+
+
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 1 = " + n);
+
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 2 = " + n);
+
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 3 = " + n);
+
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 4 = " + n);
+
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 5 = " + n);
+
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 6 = " + n);
+        if ((n % 2) == 0 && n != 1) {
+            double div2 = n / 2;
+            n = n - div2;
+
+        }else if ((n % 3) == 0 && n != 1){
+            double div3 = n / 3;
+            n = n - div3;
+
+        }else if ((n % 5) == 0 && n != 1){
+            double div5 = n / 5;
+            n = n - div5;
+
+        }else if ((n % 7) == 0 && n != 1){
+            double div7 = n / 7;
+            n = n - div7;
+
+        }else {
+            n = n - 1;
+        }
+        System.out.println("Баланс после снятия день 7 = " + n);
+
+    }
+}
---
+++ src/lesson06/Task5.java
@@ -0,0 +1,12 @@
+package lesson06;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        DayOfMoney dayOfMoney = new DayOfMoney();
+
+        dayOfMoney.MoneyFlow(21);
+
+
+    }
+}
