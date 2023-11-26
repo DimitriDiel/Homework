afce02b3b4972aadea1da6890f9a9b56f33c1e06
diel <diel-d@web.de>
2023-11-26 20:24:53 +0100

    h8

--- src/lesson08/task1/Task1.java
+++ src/lesson08/task1/Task1.java
@@ -7,30 +7,10 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Util util = new Util();
 
-        System.out.println("Please enter your array size: ");
-        int arraySize = scanner.nextInt();
+        util.intArrayRotate();
 
-        int[] intArray = new int[arraySize];
-
-        for (int i = 0; i < intArray.length; i++) {
-
-            System.out.println("Please enter " + i + " element of array");
-            intArray[i] = scanner.nextInt();
-        }
-
-        System.out.println(Arrays.toString(intArray));
-
-        int[] intArrayResult = new int[intArray.length];
-
-
-        for (int i = 0; i < intArray.length; i++) {
-
-            intArrayResult[i] = intArray[intArray.length - (i+1)];
-
-        }
-        System.out.println(Arrays.toString(intArrayResult));
 
     }
 }
