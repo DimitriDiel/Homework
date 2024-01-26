32eb64f3a1eb7792fd9d6a7ab322983f9d32d7e6
diel <diel-d@web.de>
2024-01-26 11:45:26 +0100

    lesson28

--- src/lesson28/carShowroom/dto/ResponseForClientFindAllCars.java
+++ src/lesson28/carShowroom/dto/ResponseForClientFindAllCars.java
@@ -1,4 +1,35 @@
 package lesson28.carShowroom.dto;
 
+import lesson28.carShowroom.dto.error.ErrorDto;
+import lesson28.carShowroom.entity.Car;
+
+import java.util.List;
+import java.util.Map;
+
 public class ResponseForClientFindAllCars {
+
+    private Map<String, Car> allCars;
+
+    private List<ErrorDto> errorsDtoList;
+
+    public ResponseForClientFindAllCars(Map<String, Car> allCars, List<ErrorDto> errorsDtoList) {
+        this.allCars = allCars;
+        this.errorsDtoList = errorsDtoList;
+    }
+
+    public Map<String, Car> getAllCars() {
+        return allCars;
+    }
+
+    public List<ErrorDto> getErrorsDtoList() {
+        return errorsDtoList;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseForClientFindAllCars{" +
+                "allCars=" + allCars +
+                ", errorsDtoList=" + errorsDtoList +
+                '}';
+    }
 }
--- src/lesson28/carShowroom/repository/CarDealer.java
+++ src/lesson28/carShowroom/repository/CarDealer.java
@@ -1,8 +1,5 @@
 package lesson28.carShowroom.repository;
-
-import lesson28.carShowroom.dto.CarDto;
 import lesson28.carShowroom.entity.Car;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,20 +10,15 @@ public class CarDealer {
     public CarDealer(Map<String, Car> cars) {
         this.cars = new HashMap<>();
     }
-
     public Map<String, Car> getCars() {
         return cars;
     }
-
-    public void addCar(String vinNumber, CarDto newCar) {
+    public void addCar(String vinNumber, Car newCar) {
         cars.put(vinNumber, new Car(newCar.getBrand(), newCar.getModel(), newCar.getYearOfIssue(), newCar.getPrice(), newCar.getRating()));
-
     }
-
     public Map<String, Car> findAll() {
         return cars;
     }
-
     public Car findCarByVinNumber(String vinNumber) {
         if (cars.containsKey(vinNumber)) {
             return cars.get(vinNumber);
@@ -40,6 +32,14 @@ public class CarDealer {
         }
         return false;
     }
-
+    public Map<String, Car> findCarByBrand(String brand){
+        Map<String,Car> carsByBrand = new HashMap<>();
+        for (Map.Entry<String,Car> entry : cars.entrySet()){
+            if(entry.getValue().getBrand().equals(brand)){
+                carsByBrand.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return carsByBrand;
+    }
 
 }
